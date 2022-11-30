e21fcad0c62ef3118feee9aa4f0b950341cca813
praveenmahasena647 <115216588+praveenmahasena647@users.noreply.github.com>
2022-11-30 13:25:03 +0530

    doing someThing

---
+++ snowpack.config.mjs
@@ -0,0 +1,5 @@
+export default {
+    plugins: [
+        '@snowpack/plugin-sass',
+    ],
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+let lines= document.querySelector('.lines')
+lines.addEventListener('click',()=>{
+    lines.classList.toggle('bar')
+    document.querySelector('.links').classList.toggle('nav-links')
+})
